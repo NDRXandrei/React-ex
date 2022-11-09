eb331c2f5a4554df8f54f85868c0e53b1461e785
Andrei <ndrx544@gmail.com>
2022-11-09 19:41:36 +0100

    aggiunto if age < 65

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,11 +3,12 @@ import React from "react";
 class Age extends React.Component {
   render() {
     const majorAge = this.props.age;
-    if(majorAge !== undefined){
+    if (majorAge > 18 && majorAge < 65) {
       return <p>Your age is {majorAge}</p>;
-    }
-    else {
-      return <p>Prop not present</p>
+    } else if (majorAge < 18) {
+      return <p>Too young </p>;
+    } else {
+      return <p>Too old</p>;
     }
   }
 }
@@ -19,7 +20,7 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name}</p>
-        <Age age={this.props.age}  />
+        <Age age={this.props.age} />
       </div>
     );
   }
