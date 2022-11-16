0e582db7138486350bfc8db7bf001e0632921945
Andrei <ndrx544@gmail.com>
2022-11-16 19:09:24 +0100

    aggiunta login

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,12 +1,42 @@
 import React from "react";
 
 export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="username"></input>
-        <input type="password" placeholder="password"></input>
-        <input type="checkbox"></input>
+        <input
+          type="text"
+          placeholder="username"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+        ></input>
+        <input
+          type="checkbox"
+          value={this.state.remember}
+          onChange={this.handleInputChange}
+        ></input>
         remember me
       </div>
     );
