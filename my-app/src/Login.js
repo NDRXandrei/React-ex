b2fd971228677e33cba78459a20e7f4642cc9e0f
Andrei <ndrx544@gmail.com>
2022-11-16 16:06:27 +0100

    aggiunto bottone

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,13 +1,50 @@
 import React from "react";
 
 export class Login extends React.Component {
+  state = { username: "", password: "", disabled: true };
+
+  handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onLogin = (e) => {
+    alert("E' stato inserito un nome: " + this.state.username);
+  };
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="username"></input>
-        <input type="password" placeholder="password"></input>
+        <input
+          type="text"
+          name="username"
+          placeholder="user"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+        ></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="pass"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+        ></input>
         <input type="checkbox"></input>
         remember me
+        <button
+          disabled={
+            this.state.username !== "" && this.state.password !== ""
+              ? false
+              : true
+          }
+          onClick={this.onLogin}
+        >
+          login
+        </button>
       </div>
     );
   }
