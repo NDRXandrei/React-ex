4bc0fb05fd204e2f47193c560c7f6dc160540c7a
Andrei <ndrx544@gmail.com>
2022-11-21 18:12:22 +0100

    fix della condizione

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -3,48 +3,59 @@ import React from "react";
 export class Login extends React.Component {
   state = { username: "", password: "", disabled: true };
 
-  handleInputChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+  // handleInputChange = (e) => {
+  //   const name = e.target.name;
+  //   const value = e.target.value;
 
-    this.setState({
-      [name]: value,
-    });
-  };
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
 
   onLogin = (e) => {
-    this.props.log()
+    this.props.log();
   };
+
+  handleFormChange(e) {
+    console.log(e.target.name);
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      if (this.state.username !== "" && this.state.password !== "") {
+        this.setState({ disabled: false });
+      }
+    });
+  }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="user"
-          value={this.state.username}
-          onChange={this.handleInputChange}
-        ></input>
-        <input
-          type="password"
-          name="password"
-          placeholder="pass"
-          value={this.state.password}
-          onChange={this.handleInputChange}
-        ></input>
-        <input type="checkbox"></input>
-        remember me
-        <button
-          disabled={
-            this.state.username !== "" && this.state.password !== ""
-              ? false
-              : true
-          }
-          onClick={this.onLogin}
-        >
-          login
-        </button>
+        <form onChange={this.handleFormChange.bind(this)}>
+          <input
+            type="text"
+            name="username"
+            placeholder="user"
+            // value={this.state.username}
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="pass"
+            // value={this.state.password}
+            // onChange={this.handleInputChange}
+          ></input>
+          <input type="checkbox"></input>
+          remember me
+          <button
+            // disabled={
+            //   this.state.username !== "" && this.state.password !== ""
+            //     ? false
+            //     : true
+            // }
+            disabled={this.state.disabled}
+            onClick={this.onLogin}
+          >
+            login
+          </button>
+        </form>
       </div>
     );
   }
