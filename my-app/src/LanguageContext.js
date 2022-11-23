8249f03f77632ecfdad976415fe4688b05d58d54
Andrei <ndrx544@gmail.com>
2022-11-23 19:28:45 +0100

    aggiunto h1

--- my-app/src/LanguageContext.js
+++ my-app/src/LanguageContext.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { createContext } from "react";
+
 export const LanguageContext = React.createContext("eng");
+console.log(LanguageContext._currentValue);
 
 export class DisplayLanguage extends React.Component {
+  state = { language: "" };
+  handleLanguage = (e) => {
+    this.setState({ language: e.target.value });
+  };
   render() {
+    console.log(this.state.language);
     return (
       <>
-        <select>
-          <option lang={this.context}>Italiano</option>
-          <option lang={this.context}>English</option>
+        <select onChange={this.handleLanguage}>
+          <option value={this.context._currentValue}>Italiano</option>
+          <option value={this.context._currentValue}>English</option>
         </select>
+        <h1>{this.state.language}</h1>
       </>
     );
   }
