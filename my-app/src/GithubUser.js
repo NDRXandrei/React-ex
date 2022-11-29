6f4e5305c457ae159b9c693adeb69558add235bc
Andrei <ndrx544@gmail.com>
2022-11-29 10:19:59 +0100

    aggiunto ritorno di funzione, errore e loading

--- my-app/src/GithubUser.js
+++ my-app/src/GithubUser.js
@@ -2,19 +2,54 @@ import React, { useEffect, useState } from "react";
 
 const useGithubUser = (user) => {
   const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetch(`https://api.github.com/users/${user}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setUsername(data.name);
+  //       console.log(data);
+  //     });
+  // }, []);
+
+  const useFetch = (user) => {
     fetch(`https://api.github.com/users/${user}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok === false) {
+          throw Error(response.status);
+        } else {
+          return response.json();
+        }
+      })
       .then((data) => {
         setUsername(data.name);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error.toString());
       });
-  }, []);
 
-  return [username];
+    return [username, loading, error];
+  };
+
+  return useFetch;
 };
-export function GithubUser(props) {
-  const user = useGithubUser(props.user);
 
-  return <div>{user}</div>;
+export function GithubUser(props) {
+  const fetch = useGithubUser();
+
+  const user = fetch(props.user);
+  console.log(user);
+  return (
+    <div>
+      <h5 style={user[1] === true ? { display: "block" } : { display: "none" }}>
+        Loading...
+      </h5>
+      <h3>{user[0]}</h3>
+      <h1>{user[2]}</h1>
+    </div>
+  );
 }
